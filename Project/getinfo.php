da6815e74e5a830048fc982fadd5e489fb7a76a9
DESKTOP-GVSNPV8\PimIsariya <isariya.pim@mail.kmutt.ac.th>
2017-05-27 15:25:46 +0700

    .

---
+++ Project/getinfo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html class="gr__egst_in_th">
+<head>
+	
+</head>
+<body data-gr-c-s-loaded="true">
+<option value="">Select One:</option>
+<option >EQB-600</option>
+<option >PROTREK PRW3100Y-1</option>
+<option >OCEANUS OCW-G1100S-1A</option>
+<option >BABY-G bga210-7B3</option>
+<option >BABY-G BGA-240L-1A</option>
+<option >G-SHOCK GA700-1A</option>
+<option >G-SHOCK MTG-G1000</option>
+<option >G-SHOCK PROTREK</option>
+<option >G-SHOCK SPORTS</option>
+<option >G-SHOCK GMAS110CM-7A2</option>
+
+</body>
+</html>
